f7500f6f59038cd86a0c175a99ce950edd28ebcd
mateusf-prog <mateus102006@hotmail.com>
2025-05-19 21:58:53 -0300

    edit App and SidebarTop components

--- front-pet-control-system/src/App.tsx
+++ front-pet-control-system/src/App.tsx
@@ -1,16 +1,11 @@
-import { Sidebar } from './components/Sidebar';
-import styles from './App.module.css';
-
 import './global.css'
+import { MainLayout } from './components/MainLayout';
+import { LoginForm } from './components/LoginForm';
 
 export default function App() {
   return (
-    <div>
-      <div className={styles.wrapper}>
-        <Sidebar name="Cao Limpinho"/>
-        <main>
-        </main>
-      </div>
-    </div>
+    <MainLayout>
+      <LoginForm />
+    </MainLayout>
   )
 }
--- front-pet-control-system/src/components/SidebarTop.tsx
+++ front-pet-control-system/src/components/SidebarTop.tsx
@@ -1,10 +1,10 @@
 import styles from './SidebarTop.module.css';
-import paw from '/src/images/pata.png';
+import logo from '/src/images/pata.png';
 
 export function SidebarTop({ name }: { name: string }) {
     return (
         <div className={styles.sidebartop}>
-            <img src={paw} alt="Pata" className={styles.paw} />
+            <img src={logo} alt="Pata" className={styles.paw} />
             <p>{name}</p>
         </div>
     );
